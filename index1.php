487b3681a7f86553fc670fa218027d99faef834d
Alyona Markina <alyona.markina.fipi@gmail.com>
2017-04-24 22:57:16 +0300

    Changes to views

--- index1.php
+++ index1.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>BeatyKyiv</title>
+<title>BeautyKyiv</title>
 
 <link rel="stylesheet" type="text/css" href="default1.css" />
 
